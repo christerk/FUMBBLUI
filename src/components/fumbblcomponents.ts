9b6ea29667c5350f3a4ac94b9d9ac8d405849f47
Christer Kaivo-oja <christer@fumbbl.com>
2023-11-21 20:53:36 +0100

    Adding sortabletable component

--- src/components/fumbblcomponents.ts
+++ src/components/fumbblcomponents.ts
@@ -1,9 +1,11 @@
 import PageHeader from './pageheader/pageheader.vue'
 import TitledPanel from './titledpanel/titledpanel.vue'
 import Die from './die/die.vue'
+import SortableTable from './sortable/sortabletable.vue'
 
 export {
+  Die,
   PageHeader,
-  TitledPanel,
-  Die
+  SortableTable,
+  TitledPanel
 }
---
+++ src/components/sortable/sortabletable.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="sortable">
+    <Sortable :list="Items" item-key="id" tag="div" :options="options" @change="onChange" @end="onEnd">
+      <template #item="{element}">
+        <div :class="{sortablerow: true, active: !element.empty}">
+          <slot :item="element"></slot>
+        </div>
+      </template>
+    </Sortable>
+  </div>
+</template>
+
+<style scoped>
+@import './sortabletable.less';
+</style>
+
+<script lang="ts">
+import { Component, Vue, toNative, Prop, Emit } from 'vue-facing-decorator'
+import { Sortable } from 'sortablejs-vue3'
+
+@Component({
+  components: {
+    Sortable
+  }  
+})
+class SortableTable extends Vue {
+  @Prop
+  Items: {empty: boolean, number: number}[] = []
+
+  public options = {
+    animation: 150,
+    handle: ".handle",
+    draggable: ".sortablerow",
+  }
+
+  public mounted() {
+    for (let i=0; i<this.Items.length; i++) {
+      if (this.Items[i].empty) {
+        this.Items[i].id = -i;
+      }
+    }
+  }
+
+  @Emit
+  public onChange(evt) {
+    let oldIndex = evt.oldDraggableIndex;
+    let newIndex = evt.newDraggableIndex;
+
+    let min = Math.min(oldIndex, newIndex);
+    let max = Math.max(oldIndex, newIndex);
+
+    for (let i=0; i<this.Items.length; i++) {
+      let newNumber = i+1;
+
+      if (i >= min && i <= max) {
+        if (newIndex > oldIndex && i >= oldIndex && i<= newIndex) newNumber--;
+        if (newIndex < oldIndex && i >= newIndex) newNumber++;
+      }
+
+      if (i == oldIndex) {
+        newNumber = newIndex+1;
+      }
+
+      this.Items[i].number = newNumber;
+    }
+
+
+  }
+
+  @Emit
+  public onEnd(evt) {
+    let item = this.Items.splice(evt.oldDraggableIndex, 1);
+    this.Items.splice(evt.newDraggableIndex, 0, item[0]);
+  }
+}
+
+export default toNative(SortableTable)
+</script>
