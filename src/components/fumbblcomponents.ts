dc118081da6c4e3d4bc1f940e7180627a30877fc
Christer Kaivo-oja <christer@fumbbl.com>
2024-03-29 15:31:42 +0100

    Minor formatting changes

--- src/components/fumbblcomponents.ts
+++ src/components/fumbblcomponents.ts
@@ -1,13 +1,7 @@
-import PageHeader from './pageheader/pageheader.vue'
-import TitledPanel from './titledpanel/titledpanel.vue'
-import Die from './die/die.vue'
-import SortableTable from './sortable/sortabletable.vue'
-import Spinner from './spinner/spinner.vue'
+import PageHeader from "./pageheader/pageheader.vue";
+import TitledPanel from "./titledpanel/titledpanel.vue";
+import Die from "./die/die.vue";
+import SortableTable from "./sortable/sortabletable.vue";
+import Spinner from "./spinner/spinner.vue";
 
-export {
-  Die,
-  PageHeader,
-  SortableTable,
-  TitledPanel,
-  Spinner
-}
+export { Die, PageHeader, SortableTable, TitledPanel, Spinner };
